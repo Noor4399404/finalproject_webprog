74694d8539da74d7eb8c398262194dcd1fcc68a2
Bjurn Overbeek <bbjoverbeek@gmail.com>
2021-05-20 15:20:58 +0200

    added canvas background and resizing

--- scripts/game_mechanics.js
+++ scripts/game_mechanics.js
@@ -18,6 +18,73 @@ function make_canvas(){
 
 }
 
+function set_background(){
+
+    var canvas = document.getElementById("game_canvas");
+    var ctx = canvas.getContext("2d");
+
+    // Add in background image (game board)
+    const background = new Image();
+    background.src = "images/game_board.jpg";
+
+    background.onload = function(){
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);   
+    }
+
+}
+
+
+function setup_game(){
+
+    // Set up necessary variables
+    var canvas = document.getElementById("game_canvas");
+    var ctx = canvas.getContext("2d");
+
+    // Give canvas a border (temporary)
+    canvas.setAttribute("style", "border:2px solid #000000;");
+
+    // Set window to correct size
+    resize_game();
+
+    add_triggers();
+
+}
+
+function resize_game(){
+
+    var canvas = document.getElementById("game_canvas");
+
+    var canvas_width = window.innerWidth * 0.97;
+    var canvas_height = window.innerHeight * 0.90;
+
+    let ratio = 1024 / 776; // Change to pixels of image
+    
+    if (canvas_height < canvas_width / ratio){
+        canvas_width = canvas_height * ratio;
+    } else {
+        canvas_height = canvas_width / ratio;
+    }
+
+    canvas.width = canvas_width;
+    canvas.height = canvas_height;
+
+    set_background();
+
+}
+
+function place_triggers(){
+
+    
+
+}
+
+
 $(function() {
-    make_canvas();
+    //make_canvas();
+    setup_game();
+
+    window.addEventListener("resize", function(){
+        resize_game();
+    });
+
 });
