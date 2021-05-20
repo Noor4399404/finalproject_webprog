d50ea8cec9db3676678ac64acbdbf894650c1843
oscar345 <o.zwagers@icloud.com>
2021-05-20 19:55:41 +0200

    the homepage is not finished yet, but some of the fundamentals have been created. The footer is copied from the gevpro project and needs to be modified to fit in.

--- tpl/body_end.php
+++ tpl/body_end.php
@@ -1,3 +1,35 @@
         </div>
+        <footer>
+            <div class="row">
+                <div class="col-4 get_files">
+                    <p>Get your scripts and subtitles here:</p>
+                    <ul>
+                        <li><a href="https://www.opensubtitles.com/en">OpenSubtitles <span class="extra_info_footer">- subtitles</span></a></li>
+                        <li><a href="https://imsdb.com/">IMSDb <span class="extra_info_footer">- movie scripts</span></a></li>
+                    </ul>
+                 </div>
+                <div class="col-4 used_modules">
+                    <p>The used modules:</p>
+                    <ul>
+                        <li><a href="https://flask.palletsprojects.com/en/1.1.x/">Flask <span class="extra_info_footer">- web development</span></a></li>
+                        <li><a href="https://docs.python.org/3/library/re.html">Regular Expressions <span class="extra_info_footer">- text editing</span></a></li>
+                        <li><a href="https://www.nltk.org/">NLTK <span class="extra_info_footer">- natural language toolkit</span></a></li>
+                        <li><a href="https://docs.python.org/3/library/difflib.html">Difflib <span class="extra_info_footer">- comparing sequences</span></a></li>
+                    </ul>
+                </div>
+                <div class="col-4 other_information">
+                    <p>Other information:</p>
+                    <ul>
+                        <li><a href="https://github.com/bbjoverbeek/Error_Group_name_not_found">GitHub <span class="extra_info_footer">- our repository</span></a></li>
+                        <li><a href="https://www.rug.nl/">RUG <span class="extra_info_footer">- university</span></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row logo_rug">
+                <div class="col-4">
+                    <a href="https://www.rug.nl/"><img src="../images/rug_logo.png"></a>
+                </div>
+            </div>
+        </footer>
     </body>
 </html>
