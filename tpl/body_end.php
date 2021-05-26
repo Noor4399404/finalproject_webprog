71c4de5ee6069aa395bc3d3ec28a1b5240f11ced
Noor4399404 <noor.otjens@gmai.com>
2021-05-26 13:55:48 +0200

    current page is not yet 'darkened'

--- tpl/body_end.php
+++ tpl/body_end.php
@@ -1,5 +1,5 @@
         </div>
-        <footer class="row wp-row p-3 d-flex flex-row bg-light mx-0" style="width: 100vw;">
+        <footer class="row wp-row p-3 d-flex flex-row mx-0" id="footer" style="width: 100vw;">
                 <div class="col-md-4">
                     <a href="https://www.rug.nl/?lang=en">
                         <img class=" mt-3 p-3" id="rug-logo-footer" width="100%" src="./images/rug_logo.png">
@@ -7,7 +7,7 @@
                 </div>
                 <div class="col-md-8 row d-flex flex-row wp-row justify-content-*-around">
                     <div class="col-md-6 mt-3 p-3">
-                        <p>Used modules</p>
+                        <p class="footer_title">Used modules</p>
                         <ul>
                             <li class="my-1"><a href=""> <span class="text-muted">- </span></a></li>
                             <li class="my-1"><a href=""> <span class="text-muted">-  </span></a></li>
@@ -15,7 +15,7 @@
                         </ul>
                     </div>
                     <div class="col-md-6 mt-3 p-3">
-                        <p>Useful links</p>
+                        <p class="footer_title">Useful links</p>
                         <ul>
                             <li class="my-1"><a href="https://github.com/Noor4399404/finalproject_webprog">Github <span class="text-muted">- the code</span></a></li>
                             <li class="my-1"><a href="https://www.rug.nl/?lang=en">Groningen University <span class="text-muted">- university</span></a></li>
--- tpl/head.php
+++ tpl/head.php
@@ -24,18 +24,18 @@ function p_print($array){
 </head>
 <body>
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light" id="navbar">
         <a class="navbar-brand" href="./index.php">WP21 Assignment 3</a>
         <ul class="navbar-nav mr-auto">
             <?php $active = $navigation['active']; ?>
             <?php foreach($navigation['items'] as $title => $url){
                 if ($title == $active){ ?>
                     <li class="nav-item active">
-                        <a class="nav-link" href="<?= $url ?>"><?= $title ?></a>
+                        <a class="nav-link text-light" href="<?= $url ?>"><?= $title ?></a>
                     </li>
                 <?php } else {?>
                     <li class="nav-item">
-                        <a class="nav-link" href="<?= $url ?>"><?= $title ?></a>
+                        <a class="nav-link text-light" href="<?= $url ?>"><?= $title ?></a>
                     </li>
                 <?php } ?>
             <?php } ?>
