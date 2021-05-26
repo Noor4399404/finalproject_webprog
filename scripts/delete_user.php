bbb4e19d95a99dc0e4ee6800a206140d242d2e28
oscar345 <o.zwagers@icloud.com>
2021-05-26 18:16:00 +0200

    the host is now able to delete users, while the other users cannot. the navbar can now collapse, wsome script files from bootstrap are included to do so.

--- scripts/delete_user.php
+++ scripts/delete_user.php
@@ -7,16 +7,32 @@ $deleteUserId = intval($_POST["deleteUserId"]);
 $activeGameSessionsFile =  file_get_contents('../data/active_sessions.json', 'r');
 $activeGameSessions = json_decode($activeGameSessionsFile, true);
 
+$message = array();
+
 foreach ($activeGameSessions as $key => $activeGameSession) {
     if ($activeGameSession["id"] === $gameId) {
-        $indexUserDeleting = array_search($userId, $activeGameSession["users"]);
-        if ($activeGameSession["users"]["isHost"]) {
-            $indexUserDeleted = array_search($deleteUserId, $activeGameSession["users"]);
+        array_push($message, "found game session");
+
+        foreach ($activeGameSession["users"] as $keySecond => $user) {
+            if ($user["id"] == $deleteUserId) {
+                array_push($message, "user is almost goneeeeee");
+                $indexUserDeleted = $key;
+            } else if ($user["id"] == $userId) {
+                $isHost = $user["isHost"];
+            }
+        }
+        if ($isHost) {
+            array_push($message, "user is goneeeeee");
             array_splice($activeGameSessions[$key]["users"], $activeGameSessions[$key]["users"][$indexUserDeleted], 1);
         }
     }
 }
 
-$answer = json_encode($currentGameInfo);
+$writableData = json_encode($activeGameSessions);
+$json_file = fopen('../data/active_sessions.json', 'w'); 
+fwrite($json_file, $writableData); 
+fclose($json_file);
+
+$answer = json_encode($message);
 echo $answer;
 ?>
--- tpl/body_end.php
+++ tpl/body_end.php
@@ -1,27 +1,31 @@
         </div>
         <footer class="row wp-row p-3 d-flex flex-row mx-0" id="footer" style="width: 100vw;">
-                <div class="col-md-4">
-                    <a href="https://www.rug.nl/?lang=en">
-                        <img class=" mt-3 p-3" id="rug-logo-footer" width="100%" src="./images/rug_logo.png">
-                    </a>
+            <div class="col-md-4">
+                <a href="https://www.rug.nl/?lang=en">
+                    <img class=" mt-3 p-3" id="rug-logo-footer" width="100%" src="./images/rug_logo.png">
+                </a>
+            </div>
+            <div class="col-md-8 row d-flex flex-row wp-row justify-content-*-around">
+                <div class="col-md-6 mt-3 p-3">
+                    <p class="footer_title">Used modules</p>
+                    <ul>
+                        <li class="my-1"><a href=""> <span class="text-muted">- </span></a></li>
+                        <li class="my-1"><a href=""> <span class="text-muted">- </span></a></li>
+                        <li class="my-1"><a href=""> <span class="text-muted">- </span></a></li>
+                    </ul>
                 </div>
-                <div class="col-md-8 row d-flex flex-row wp-row justify-content-*-around">
-                    <div class="col-md-6 mt-3 p-3">
-                        <p class="footer_title">Used modules</p>
-                        <ul>
-                            <li class="my-1"><a href=""> <span class="text-muted">- </span></a></li>
-                            <li class="my-1"><a href=""> <span class="text-muted">-  </span></a></li>
-                            <li class="my-1"><a href=""> <span class="text-muted">-  </span></a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-6 mt-3 p-3">
-                        <p class="footer_title">Useful links</p>
-                        <ul>
-                            <li class="my-1"><a href="https://github.com/Noor4399404/finalproject_webprog">Github <span class="text-muted">- the code</span></a></li>
-                            <li class="my-1"><a href="https://www.rug.nl/?lang=en">Groningen University <span class="text-muted">- university</span></a></li>
-                        </ul>
-                    </div>
+                <div class="col-md-6 mt-3 p-3">
+                    <p class="footer_title">Useful links</p>
+                    <ul>
+                        <li class="my-1"><a href="https://github.com/Noor4399404/finalproject_webprog">Github <span class="text-muted">- the code</span></a></li>
+                        <li class="my-1"><a href="https://www.rug.nl/?lang=en">Groningen University <span class="text-muted">- university</span></a></li>
+                    </ul>
                 </div>
-            </footer>
-    </body>
-</html>
+            </div>
+        </footer>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+
+        </body>
+
+        </html>
