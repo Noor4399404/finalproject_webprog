888b0b05477212aa277f24bc84ae41e22c9fd3be
Noor4399404 <noor.otjens@gmai.com>
2021-05-26 13:19:56 +0200

    Added pages to navigation bar

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $page_title = 'Webprogramming Final assignment';
 $navigation = array(
     'active' => 'Home',
     'items' => array(
+        'Home' => '/WP21/finalproject_webprog/index.php',
         'How To Play' => '/WP21/finalproject_webprog/game_rules.php',
         'Test_movement' => '/WP21/finalproject_webprog/test_movement.php'
     )
