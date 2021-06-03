4a9b18842137cce96811d19c7c9f700e62f51a63
Dennis4472373 <d.l.van.thulden@student.rug.nl>
2021-06-03 13:40:26 +0200

    Improved game interface

--- game.php
+++ game.php
@@ -15,7 +15,7 @@ include __DIR__ . '/tpl/head.php';
 $json_file = file_get_contents("data/trigger_locations.json");
 
 ?>
-<div class="d-flex p-2">
+<div class="d-flex p-4" id="gamebody">
     <div class="d-flex flex-row">
         <div class="d-flex flex-column">
             <div class="container-fluid">
@@ -25,12 +25,13 @@ $json_file = file_get_contents("data/trigger_locations.json");
         </div>
         <div class="d-flex flex-column">
             <div class="d-flex flex-row">
-                <div class="d-flex flex-column">
+                <div class="d-flex flex-column mt-3">
                     <h5>X more moves until Mr. X reveals himself!</h5>
                 </div>
             </div>
             <div class="d-flex flex-row">
-                <div class="d-flex flex-column">
+                <div class="d-flex flex-column mt-4">
+                    <h5>Other players moves</h5>
                     <table class="table" id="moves_table">
                         <thead>
                             <tr>
@@ -67,194 +68,197 @@ $json_file = file_get_contents("data/trigger_locations.json");
                     </table>
                 </div>
             </div>
-            <div class="d-flex flex-row justify-content-center" id="vehiclebuttons">
-                <div class="d-flex flex-column m-3">
-                    <input type="button" class="btn btn-warning btn-block" value="Taxi: 10">
-                </div>
-                <div class="d-flex flex-column m-3">
-                    <input type="button" class="btn btn-danger btn-block" value="Bus: 8">
-                </div>
-                <div class="d-flex flex-column m-3">
-                    <input type="button" class="btn btn-dark btn-block" value="Underground: 4">
-                </div>
-            </div>
-            <div class="d-flex flex-row justify-content-center">
-                <div class="d-flex flex-column m-5 col-12">
-                    <input type="submit" class="btn btn-success" value="Submit Move">
-                </div>
-            </div>
             <div class="d-flex flex-row">
                 <div class="d-flex flex-column">
                     <h5>Mr. X used vehicles</h5>
                     <div id="mrxdiv">
                         <table class="table" id="mrxtable">
                             <thead>
-                                <th scope="col">
-                                    Round
-                                </th>
-                                <th scope="col">
-                                    Vehicle
-                                </th>
+                            <th scope="col">
+                                Round
+                            </th>
+                            <th scope="col">
+                                Vehicle
+                            </th>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <th scope="row">
-                                        1
-                                    </th>
-                                    <td>Taxi</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        2
-                                    </th>
-                                    <td>Bus</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        3
-                                    </th>
-                                    <td>Bus</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        4
-                                    </th>
-                                    <td>Underground</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        5
-                                    </th>
-                                    <td>Taxi</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        6
-                                    </th>
-                                    <td>Taxi</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        7
-                                    </th>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        8
-                                    </th>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        9
-                                    </th>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        10
-                                    </th>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        11
-                                    </th>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        12
-                                    </th>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        13
-                                    </th>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        14
-                                    </th>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        15
-                                    </th>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        16
-                                    </th>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        17
-                                    </th>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        18
-                                    </th>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        19
-                                    </th>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        20
-                                    </th>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        21
-                                    </th>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        22
-                                    </th>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        23
-                                    </th>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        24
-                                    </th>
-                                    <td></td>
-                                </tr>
+                            <tr>
+                                <th scope="row">
+                                    1
+                                </th>
+                                <td>Taxi</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    2
+                                </th>
+                                <td>Bus</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    3
+                                </th>
+                                <td>Bus</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    4
+                                </th>
+                                <td>Underground</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    5
+                                </th>
+                                <td>Taxi</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    6
+                                </th>
+                                <td>Taxi</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    7
+                                </th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    8
+                                </th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    9
+                                </th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    10
+                                </th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    11
+                                </th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    12
+                                </th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    13
+                                </th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    14
+                                </th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    15
+                                </th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    16
+                                </th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    17
+                                </th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    18
+                                </th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    19
+                                </th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    20
+                                </th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    21
+                                </th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    22
+                                </th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    23
+                                </th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    24
+                                </th>
+                                <td></td>
+                            </tr>
                             </tbody>
                         </table>
                     </div>
                 </div>
-                <div class="d-flex flex-column">
-                    <div class="d-flex flex-row justify-content-center">
-                        <div class="d-flex flex-column align-self-center">
-                            <input type="button" class="btn btn-danger" value="End Game">
-                        </div>
+            </div>
+            <div class="d-flex flex-row justify-content-center mt-5" id="vehiclebuttons">
+                <div class="d-flex flex-row"></div>
+                    <div class="d-flex flex-column">
+                        <h5>Choose your vehicle:</h5>
                     </div>
-                    <div class="d-flex flex-row justify-content-center">
-                        <div class="d-flex flex-column align-self-center">
-                            <input type="button" class="btn btn-primary" value="?">
-                        </div>
+                </div>
+                <div class="d-flex flex-row mt-3">
+                    <div class="d-flex flex-column ml-2 mr-2">
+                        <input type="button" class="btn btn-warning btn-lg" value="Taxi: 10">
+                    </div>
+                    <div class="d-flex flex-column ml-2 mr-2">
+                        <input type="button" class="btn btn-danger btn-lg" value="Bus: 8">
+                    </div>
+                    <div class="d-flex flex-column ml-2 mr-2">
+                        <input type="button" class="btn btn-dark btn-lg" value="Underground: 4">
+                    </div>
+                </div>
+                <div class="d-flex flex-row mt-3">
+                    <div class="d-flex flex-column col-12">
+                        <input type="button" class="btn btn-success" value="Submit Move">
+                    </div>
+                </div>
+                <div class="d-flex flex-row justify-content-center mt-5">
+                    <div class="d-flex flex-column ml-2 mr-2">
+                        <input type="button" class="btn btn-danger" value="End Game">
+                    </div>
+                    <div class="d-flex flex-column ml-2 mr-2">
+                        <input type="button" class="btn btn-primary" value="?">
                     </div>
                 </div>
             </div>
