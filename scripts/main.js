7adf7ee3d6ac06746c1b3214721a6f352d483955
Dennis4472373 <d.l.van.thulden@student.rug.nl>
2021-06-06 15:08:09 +0200

    The card amount table now fills up with the correct data. The moves buttons as well. Only, the numbers are not formatted correctly yet.

--- scripts/main.js
+++ scripts/main.js
@@ -22,9 +22,34 @@ function useModal(modalTitle, modalText, closeButtonText, closeModalAction = () 
 function fillUsernames() {
     $.getJSON("data/test_sessions.json", function (data) {
         let headingRow = $('#moves_table tr')[0];
+        let rows = $('#moves_table tbody tr');
+
+        let firstRow = rows[0].childNodes;
+        let secondRow = rows[1].childNodes;
+        let thirdRow = rows[2].childNodes;
+
+        let vehicleButtons = $('.vehicle_button_div p');
+        console.log(vehicleButtons);
+
         for (let key in data) {
-            console.log(data[key]['users'][0]['userName']);
-            headingRow.innerHTML = '<th scope="col">' + 'Vehicles' + '</th><th scope="col">' + data[key]['users'][0]['userName'] + '</th><th scope="col">' + data[key]['users'][1]['userName'] + '</th><th scope="col">' + data[key]['users'][2]['userName'] + '</th>';
+            headingRow.innerHTML = '<th scope="col">' + 'Vehicles' + '</th><th scope="col">' + data[key]['users'][0]['username'] + '</th><th scope="col">' + data[key]['users'][1]['username'] + '</th><th scope="col">' + data[key]['users'][2]['username'] + '</th>';
+
+            firstRow[3].innerHTML = '<td>' + data[key]['users'][0]['cardAmount']['tax'] + '</td>';
+            firstRow[5].innerHTML = '<td>' + data[key]['users'][1]['cardAmount']['tax'] + '</td>';
+            firstRow[7].innerHTML = '<td>' + data[key]['users'][2]['cardAmount']['tax'] + '</td>';
+
+            secondRow[3].innerHTML = '<td>' + data[key]['users'][0]['cardAmount']['bus'] + '</td>';
+            secondRow[5].innerHTML = '<td>' + data[key]['users'][1]['cardAmount']['bus'] + '</td>';
+            secondRow[7].innerHTML = '<td>' + data[key]['users'][2]['cardAmount']['bus'] + '</td>';
+
+            thirdRow[3].innerHTML = '<td>' + data[key]['users'][0]['cardAmount']['und'] + '</td>';
+            thirdRow[5].innerHTML = '<td>' + data[key]['users'][1]['cardAmount']['und'] + '</td>';
+            thirdRow[7].innerHTML = '<td>' + data[key]['users'][2]['cardAmount']['und'] + '</td>';
+
+            vehicleButtons[0].innerHTML = '<p>' + data[key]['users'][3]['cardAmount']['tax'] + '</p>';
+            vehicleButtons[1].innerHTML = '<p>' + data[key]['users'][3]['cardAmount']['bus'] + '</p>';
+            vehicleButtons[2].innerHTML = '<p>' + data[key]['users'][3]['cardAmount']['und'] + '</p>';
+
         }
     })
 }
