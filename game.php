b2f33d6c1233925abc1796c346da2537a0d71f0b
Dennis4472373 <d.l.van.thulden@student.rug.nl>
2021-06-08 15:50:20 +0200

    Table data en button data now correspond with the active user

--- game.php
+++ game.php
@@ -220,19 +220,6 @@ $session_data = json_decode($session_data_json, true);
                                 Vehicle
                             </th>
                             <th scope="col">
-                                User 1
-                            </th>
-                            <th scope="col">
-                                User 2
-                            </th>
-                            <th scope="col">
-                                Mr. X
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>
                                 <div class="border-warning border vehicle_info_div">
                                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff">
                                         <path d="M0 0h24v24H0V0z" fill="none" />
@@ -241,20 +228,8 @@ $session_data = json_decode($session_data_json, true);
                                         <circle cx="16.5" cy="14.5" r="1.5" />
                                     </svg>
                                 </div>
-                            </td>
-                            <td>
-                                <p>24</p>
-                            </td>
-                            <td>
-                                <p>24</p>
-
-                            </td>
-                            <td>
-                                <p>24</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
+                            </th>
+                            <th scope="col">
                                 <div class="border-info border vehicle_info_div">
                                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff">
                                         <path d="M0 0h24v24H0V0z" fill="none" />
@@ -263,38 +238,18 @@ $session_data = json_decode($session_data_json, true);
                                         <circle cx="15.5" cy="14.5" r="1.5" />
                                     </svg>
                                 </div>
-                            </td>
-                            <td>
-                                <p>24</p>
-                            </td>
-                            <td>
-                                <p>24</p>
-
-                            </td>
-                            <td>
-                                <p>24</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
+                            </th>
+                            <th scope="col">
                                 <div class="border-danger border vehicle_info_div">
                                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff">
                                         <path d="M0 0h24v24H0V0z" fill="none" />
                                         <path d="M17.8 2.8C16 2.09 13.86 2 12 2s-4 .09-5.8.8C3.53 3.84 2 6.05 2 8.86V22h20V8.86c0-2.81-1.53-5.02-4.2-6.06zM9.17 20l1.5-1.5h2.66l1.5 1.5H9.17zm-2.16-6V9h10v5h-10zm9.49 2c0 .55-.45 1-1 1s-1-.45-1-1 .45-1 1-1 1 .45 1 1zm-8-1c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zM20 20h-3.5v-.38l-1.15-1.16c1.49-.17 2.65-1.42 2.65-2.96V9c0-2.63-3-3-6-3s-6 .37-6 3v6.5c0 1.54 1.16 2.79 2.65 2.96L7.5 19.62V20H4V8.86c0-2 1.01-3.45 2.93-4.2C8.41 4.08 10.32 4 12 4s3.59.08 5.07.66c1.92.75 2.93 2.2 2.93 4.2V20z" />
                                     </svg>
                                 </div>
-                            </td>
-                            <td>
-                                <p>24</p>
-                            </td>
-                            <td>
-                                <p>24</p>
-
-                            </td>
-                            <td>
-                                <p>24</p>
-                            </td>
+                            </th>
                         </tr>
+                    </thead>
+                    <tbody>
                     </tbody>
                 </table>
             </div>
--- scripts/main.js
+++ scripts/main.js
@@ -22,41 +22,26 @@ function useModal(modalTitle, modalText, closeButtonText, closeModalAction = () 
 function fillData() {
     window.sessionStorage.setItem("userId", "1234567");
     $.getJSON("data/test_sessions.json", function (data) {
-        let headingRow = $('#moves_table tr')[0];
-        let rows = $('#moves_table tbody tr');
-
-        let firstRow = rows[0].childNodes;
-        let secondRow = rows[1].childNodes;
-        let thirdRow = rows[2].childNodes;
-
         let vehicleButtons = $('#move_buttons > p');
-
         for (let key in data) {
             for (let user in data[key]['users']) {
+                 if (data[key]['users'][user]['id'] !== window.sessionStorage.getItem("userId")) {
+                     $('#moves_table tbody').append('<tr></tr>');
+                     let correctRow = $('#moves_table tbody tr').last();
+                     correctRow.append('<td>' + data[key]["users"][user]["username"] + '</td>');
+                     correctRow.append('<td>' + data[key]["users"][user]["cardAmount"]["tax"] + '</td>');
+                     correctRow.append('<td>' + data[key]["users"][user]["cardAmount"]["bus"] + '</td>');
+                     correctRow.append('<td>' + data[key]["users"][user]["cardAmount"]["und"] + '</td>');
+
+                 }
                  if (data[key]['users'][user]['id'] === window.sessionStorage.getItem("userId")) {
-                     let activeUser = data[key]['users'][user];
-                     console.log(activeUser);
+                     vehicleButtons[0].innerHTML = '<p class="mb-0 text-white">' + data[key]['users'][user]['cardAmount']['tax'] + '</p>';
+                     vehicleButtons[1].innerHTML = '<p class="mb-0 text-white">' + data[key]['users'][user]['cardAmount']['bus'] + '</p>';
+                     vehicleButtons[2].innerHTML = '<p class="mb-0 text-white">' + data[key]['users'][user]['cardAmount']['und'] + '</p>';
+
+                     $('#station').text('Current Location: ' + data[key]['users'][user]['location']);
                  }
             }
-            headingRow.innerHTML = '<th scope="col">' + 'Vehicles' + '</th><th scope="col">' + data[key]['users'][0]['username'] + '</th><th scope="col">' + data[key]['users'][1]['username'] + '</th><th scope="col">' + data[key]['users'][2]['username'] + '</th>';
-
-            firstRow[3].innerHTML = '<td>' + data[key]['users'][0]['cardAmount']['tax'] + '</td>';
-            firstRow[5].innerHTML = '<td>' + data[key]['users'][1]['cardAmount']['tax'] + '</td>';
-            firstRow[7].innerHTML = '<td>' + data[key]['users'][2]['cardAmount']['tax'] + '</td>';
-
-            secondRow[3].innerHTML = '<td>' + data[key]['users'][0]['cardAmount']['bus'] + '</td>';
-            secondRow[5].innerHTML = '<td>' + data[key]['users'][1]['cardAmount']['bus'] + '</td>';
-            secondRow[7].innerHTML = '<td>' + data[key]['users'][2]['cardAmount']['bus'] + '</td>';
-
-            thirdRow[3].innerHTML = '<td>' + data[key]['users'][0]['cardAmount']['und'] + '</td>';
-            thirdRow[5].innerHTML = '<td>' + data[key]['users'][1]['cardAmount']['und'] + '</td>';
-            thirdRow[7].innerHTML = '<td>' + data[key]['users'][2]['cardAmount']['und'] + '</td>';
-
-            vehicleButtons[0].innerHTML = '<p class="mb-0 text-white">' + data[key]['users'][3]['cardAmount']['tax'] + '</p>';
-            vehicleButtons[1].innerHTML = '<p class="mb-0 text-white">' + data[key]['users'][3]['cardAmount']['bus'] + '</p>';
-            vehicleButtons[2].innerHTML = '<p class="mb-0 text-white">' + data[key]['users'][3]['cardAmount']['und'] + '</p>';
-
-            $('#station').text('Current Location: ' + data[key]['users'][3]['location']);
         }
     })
 }
