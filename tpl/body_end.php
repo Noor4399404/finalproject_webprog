5d3695b1b88f76d569820eef9a89db4189ca28fe
Bjurn Overbeek <bbjoverbeek@gmail.com>
2021-06-11 16:30:20 +0200

    fixed footer height

--- tpl/body_end.php
+++ tpl/body_end.php
@@ -1,5 +1,5 @@
         </div>
-        <footer class="row wp-row p-3 d-flex flex-row mx-0" id="footer" style="width: 100vw;">
+        <footer class="row wp-row p-3 d-flex flex-row mx-0 mt-auto" id="footer" style="width: 100vw;">
             <div class="col-md-4">
                 <a href="https://www.rug.nl/?lang=en">
                     <img class=" mt-3 p-3" id="rug-logo-footer" width="100%" src="./images/rug_logo.png">
