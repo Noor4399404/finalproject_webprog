3c3bae78afaa4bec0ff0c2c18779d984f7e97549
oscar345 <o.zwagers@icloud.com>
2021-06-12 20:00:16 +0200

    removed submit button

--- game.php
+++ game.php
@@ -30,7 +30,8 @@ $json_file = file_get_contents("data/trigger_locations.json");
             </div>
             <div class="modal-body">
                 <div class="instructions">
-                    <br><h2>These are the rules!</h2><br>
+                    <br>
+                    <h2>These are the rules!</h2><br>
 
                     <p>In the center of london it is not difficult to hide in the traffic. If anyone wants to make himself untraceable here, it takes the best detectives of Scotland Yars detectives to perhaps still track him down. <br>One of the players is 'Mister X', who is hiding somewhere on his flight across London. He moves "invisibly" and only shows himself after covering a certain amount of cards. All the other players are Scotland Yard's detectives and try to track him down. </p>
 
@@ -41,7 +42,7 @@ $json_file = file_get_contents("data/trigger_locations.json");
                         while the detectives try to pick up his trail and track him down. <br> One person will be Mister X. The host can assign this position, to one of the participants of the game.</p>
 
                     <h3>Goal of the game</h3><br>
-                    <p>If a detective manages to get to the same place as the invisible "Mister X" , then "Mister X" must show himself and the detectives  win. However, if "Mister X" manages to remain undetected until the detectives have used  all their tickets and thus all their moves, "Mister X" has won.
+                    <p>If a detective manages to get to the same place as the invisible "Mister X" , then "Mister X" must show himself and the detectives win. However, if "Mister X" manages to remain undetected until the detectives have used all their tickets and thus all their moves, "Mister X" has won.
                         <br> A start position is randomly given to you.</> <br>
 
                     <h3>Preparation</h3><br>
@@ -58,7 +59,8 @@ $json_file = file_get_contents("data/trigger_locations.json");
                         <li>3 bus tickets</li>
                         <li>3 underground tickets</li>
                         <li>2 double tickets</li>
-                    </ul></p>
+                    </ul>
+                    </p>
 
                     Every time a detective uses a ticket, this ticket is given to Mister X.
 
@@ -67,39 +69,31 @@ $json_file = file_get_contents("data/trigger_locations.json");
 
 
 
-                    <Br><h3>Transportation</h3><br>
+                    <Br>
+                    <h3>Transportation</h3><br>
                     <p>Every move is a ride on the bus, the underground or the famous london taxi. For such a ride, a matching ticket must be handed to Mister X. The traffic lines drawn on the game board correspond in color to the tickets. Each point is a stop for one or more means of transport. The colors of the stops indicate which means of transport may stop there.</p>
 
                     <p class="tip"> Tip: The numbers of the stations on the game board
                         are arranged from left to right to make it easier to find your starting positions.</p><br>
-                    <div class="one_piece">    <figure>
-                            <img src="images/taxi_station.jpg"
-                                 alt="A station on the Scotland Yard game board."
-                                 title="Scotland Yard Station"
-                                 width="175"
-                                 height="200"
-                            />
+                    <div class="one_piece">
+                        <figure>
+                            <img src="images/taxi_station.jpg" alt="A station on the Scotland Yard game board." title="Scotland Yard Station" width="175" height="200" />
                             <figcaption><i>Station 1</i></figcaption>
-                        </figure></div>
-                    <div class="one_piece">    <figure>
-                            <img src="images/bus_station.jpg"
-                                 alt="A station on the Scotland Yard game board."
-                                 title="Scotland Yard Station"
-                                 width="175"
-                                 height="200"
-                            />
+                        </figure>
+                    </div>
+                    <div class="one_piece">
+                        <figure>
+                            <img src="images/bus_station.jpg" alt="A station on the Scotland Yard game board." title="Scotland Yard Station" width="175" height="200" />
                             <figcaption><i>Station 2</i></figcaption>
-                        </figure></div>
-                    <div class="one_piece">    <figure>
-                            <img src="images/one_piece.jpg"
-                                 alt="A station on the Scotland Yard game board."
-                                 title="Scotland Yard Station"
-                                 width="175"
-                                 height="200"
-                            />
+                        </figure>
+                    </div>
+                    <div class="one_piece">
+                        <figure>
+                            <img src="images/one_piece.jpg" alt="A station on the Scotland Yard game board." title="Scotland Yard Station" width="175" height="200" />
                             <figcaption><i>Station 3</i></figcaption>
-                        </figure></div>
-                    <p>Every station is a taxi station (<i>station 1</i>). The bus  only drives from stations with a turquoise semi-circle (<i>station 2</i>). A bus will take you a little further than the taxi (along the bus line).<br>
+                        </figure>
+                    </div>
+                    <p>Every station is a taxi station (<i>station 1</i>). The bus only drives from stations with a turquoise semi-circle (<i>station 2</i>). A bus will take you a little further than the taxi (along the bus line).<br>
 
                         The underground travels along the red line and covers the furthest distances the quickest. However, there are only a few underground stations (<i>station 3</i>) on the map.<br>
 
@@ -141,20 +135,25 @@ $json_file = file_get_contents("data/trigger_locations.json");
                         </button>
                     </div>
                     <p><span class="transport_explained">Taxi button</span> <span class="transport_explained">Bus button</span> <span class="transport_explained">Underground button</span></p>
-                    <br><p>With these buttons it is possible to move on te board. You cannot always use every button. It depends on which station you are positioned. And be aware of how many buttons you have left. You do not have infinite buttons. </p>
+                    <br>
+                    <p>With these buttons it is possible to move on te board. You cannot always use every button. It depends on which station you are positioned. And be aware of how many buttons you have left. You do not have infinite buttons. </p>
                     <p>When you are sure you want to use the button, you can fix your position with the submit button.</p>
                     <div class="my-3">
                         <button type="button" style="width: 20%;" class="btn btn-success">Submit Move</button>
                     </div>
                     <div class="misterX">
-                        </b><h3> Mister X</h3><br>
+                        </b>
+                        <h3> Mister X</h3><br>
                         <p>Mister X always makes the first move. This is not visible to the detectives. The means of transport is visible to the detectives.
                             Mister X has several special moves:<br>
-                            <Br><b>Appearing</b>, The location of "Mister X"  is shown regularly: the third, eighth, thirteenth, eighteenth and the last move.<br>
+                            <Br><b>Appearing</b>, The location of "Mister X" is shown regularly: the third, eighth, thirteenth, eighteenth and the last move.<br>
                             <br><b>Double move</b>, Mister X has two double cards. When playing such a card, he can choose a combination of two different means of transport. This double card is counted as two individual moves and he will also gives two tickets. If mister X must appear in the first move, he must show up there. With the second move, he immediately disappears again.<Br>
                             <Br><b>Black tickets</b>, Mister X may use his black tickets every time it is his turn instead of the normal tickets (also with a double move).<br>
-                            The black ticket applies to every means of transport and therefore means a black day for the detectives, because when using the card they do not receive any information about the means of transport that Mister X used. The black ticket is also the only ticket that can be used to travel by boat.</p></div>
-                    <br><p><b>Good Luck!</b></p>
+                            The black ticket applies to every means of transport and therefore means a black day for the detectives, because when using the card they do not receive any information about the means of transport that Mister X used. The black ticket is also the only ticket that can be used to travel by boat.
+                        </p>
+                    </div>
+                    <br>
+                    <p><b>Good Luck!</b></p>
                 </div>
             </div>
             <div class="modal-footer">
@@ -270,7 +269,7 @@ $session_data = json_decode($session_data_json, true);
                                     1
                                 </th>
                                 <td>
-                                    
+
                                 </td>
                             </tr>
                             <tr>
@@ -278,7 +277,7 @@ $session_data = json_decode($session_data_json, true);
                                     2
                                 </th>
                                 <td>
-                                   
+
                                 </td>
                             </tr>
                             <tr>
@@ -286,7 +285,7 @@ $session_data = json_decode($session_data_json, true);
                                     3
                                 </th>
                                 <td>
-                                    
+
                                 </td>
                             </tr>
                             <tr>
@@ -294,7 +293,7 @@ $session_data = json_decode($session_data_json, true);
                                     4
                                 </th>
                                 <td>
-                                    
+
                                 </td>
                             </tr>
                             <tr>
@@ -302,7 +301,7 @@ $session_data = json_decode($session_data_json, true);
                                     5
                                 </th>
                                 <td>
-                                    
+
                                 </td>
                             </tr>
                             <tr>
@@ -310,7 +309,7 @@ $session_data = json_decode($session_data_json, true);
                                     6
                                 </th>
                                 <td>
-                                    
+
                                 </td>
                             </tr>
                             <tr>
@@ -425,42 +424,45 @@ $session_data = json_decode($session_data_json, true);
                     </table>
                 </div>
             </div>
-            <div id="make-move-div" class="p-2 d-flex flex-column">
-                <h4 class="text-center">Make move</h4>
-                <div class="d-flex flex-row justify-content-around">
-                    <button class="vehicle_buttons btn-warning" id="move_buttons">
-                        <div class="vehicle_button_div vehicle_info_div" id="tax_button">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff">
-                                <path d="M0 0h24v24H0V0z" fill="none" />
-                                <path d="M18.92 6.01C18.72 5.42 18.16 5 17.5 5H15V3H9v2H6.5c-.66 0-1.21.42-1.42 1.01L3 12v8c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h12v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-8l-2.08-5.99zM6.85 7h10.29l1.04 3H5.81l1.04-3zM19 17H5v-4.66l.12-.34h13.77l.11.34V17z" />
-                                <circle cx="7.5" cy="14.5" r="1.5" />
-                                <circle cx="16.5" cy="14.5" r="1.5" />
-                            </svg>
-                            <p>24</p>
-                        </div>
-                    </button>
-                    <button class="vehicle_buttons btn-info" id="move_buttons">
-                        <div class="vehicle_button_div vehicle_info_div" id="bus_button">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff">
-                                <path d="M0 0h24v24H0V0z" fill="none" />
-                                <path d="M12 2c-4.42 0-8 .5-8 4v10c0 .88.39 1.67 1 2.22V20c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h8v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1.78c.61-.55 1-1.34 1-2.22V6c0-3.5-3.58-4-8-4zm5.66 2.99H6.34C6.89 4.46 8.31 4 12 4s5.11.46 5.66.99zm.34 2V10H6V6.99h12zm-.34 9.74l-.29.27H6.63l-.29-.27C6.21 16.62 6 16.37 6 16v-4h12v4c0 .37-.21.62-.34.73z" />
-                                <circle cx="8.5" cy="14.5" r="1.5" />
-                                <circle cx="15.5" cy="14.5" r="1.5" />
-                            </svg>
-                            <p>24</p>
-                        </div>
-                    </button>
-                    <button class="vehicle_buttons btn-danger" id="move_buttons">
-                        <div class="vehicle_button_div vehicle_info_div" id="und_button">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff">
-                                <path d="M0 0h24v24H0V0z" fill="none" />
-                                <path d="M17.8 2.8C16 2.09 13.86 2 12 2s-4 .09-5.8.8C3.53 3.84 2 6.05 2 8.86V22h20V8.86c0-2.81-1.53-5.02-4.2-6.06zM9.17 20l1.5-1.5h2.66l1.5 1.5H9.17zm-2.16-6V9h10v5h-10zm9.49 2c0 .55-.45 1-1 1s-1-.45-1-1 .45-1 1-1 1 .45 1 1zm-8-1c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zM20 20h-3.5v-.38l-1.15-1.16c1.49-.17 2.65-1.42 2.65-2.96V9c0-2.63-3-3-6-3s-6 .37-6 3v6.5c0 1.54 1.16 2.79 2.65 2.96L7.5 19.62V20H4V8.86c0-2 1.01-3.45 2.93-4.2C8.41 4.08 10.32 4 12 4s3.59.08 5.07.66c1.92.75 2.93 2.2 2.93 4.2V20z" />
-                            </svg>
-                            <p>24</p>
-                        </div>
-                    </button>
+            <div>
+                <div id="make-move-div" class="p-2 flex-column" style="display: flex;">
+                    <h4 class="text-center">Make move</h4>
+                    <div class="d-flex flex-row justify-content-around">
+                        <button class="vehicle_buttons btn-warning" id="move_buttons">
+                            <div class="vehicle_button_div vehicle_info_div" id="tax_button">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff">
+                                    <path d="M0 0h24v24H0V0z" fill="none" />
+                                    <path d="M18.92 6.01C18.72 5.42 18.16 5 17.5 5H15V3H9v2H6.5c-.66 0-1.21.42-1.42 1.01L3 12v8c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h12v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-8l-2.08-5.99zM6.85 7h10.29l1.04 3H5.81l1.04-3zM19 17H5v-4.66l.12-.34h13.77l.11.34V17z" />
+                                    <circle cx="7.5" cy="14.5" r="1.5" />
+                                    <circle cx="16.5" cy="14.5" r="1.5" />
+                                </svg>
+                                <p>24</p>
+                            </div>
+                        </button>
+                        <button class="vehicle_buttons btn-info" id="move_buttons">
+                            <div class="vehicle_button_div vehicle_info_div" id="bus_button">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff">
+                                    <path d="M0 0h24v24H0V0z" fill="none" />
+                                    <path d="M12 2c-4.42 0-8 .5-8 4v10c0 .88.39 1.67 1 2.22V20c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h8v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1.78c.61-.55 1-1.34 1-2.22V6c0-3.5-3.58-4-8-4zm5.66 2.99H6.34C6.89 4.46 8.31 4 12 4s5.11.46 5.66.99zm.34 2V10H6V6.99h12zm-.34 9.74l-.29.27H6.63l-.29-.27C6.21 16.62 6 16.37 6 16v-4h12v4c0 .37-.21.62-.34.73z" />
+                                    <circle cx="8.5" cy="14.5" r="1.5" />
+                                    <circle cx="15.5" cy="14.5" r="1.5" />
+                                </svg>
+                                <p>24</p>
+                            </div>
+                        </button>
+                        <button class="vehicle_buttons btn-danger" id="move_buttons">
+                            <div class="vehicle_button_div vehicle_info_div" id="und_button">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff">
+                                    <path d="M0 0h24v24H0V0z" fill="none" />
+                                    <path d="M17.8 2.8C16 2.09 13.86 2 12 2s-4 .09-5.8.8C3.53 3.84 2 6.05 2 8.86V22h20V8.86c0-2.81-1.53-5.02-4.2-6.06zM9.17 20l1.5-1.5h2.66l1.5 1.5H9.17zm-2.16-6V9h10v5h-10zm9.49 2c0 .55-.45 1-1 1s-1-.45-1-1 .45-1 1-1 1 .45 1 1zm-8-1c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zM20 20h-3.5v-.38l-1.15-1.16c1.49-.17 2.65-1.42 2.65-2.96V9c0-2.63-3-3-6-3s-6 .37-6 3v6.5c0 1.54 1.16 2.79 2.65 2.96L7.5 19.62V20H4V8.86c0-2 1.01-3.45 2.93-4.2C8.41 4.08 10.32 4 12 4s3.59.08 5.07.66c1.92.75 2.93 2.2 2.93 4.2V20z" />
+                                </svg>
+                                <p>24</p>
+                            </div>
+                        </button>
+                    </div>
                 </div>
             </div>
+
         </div>
     </div>
 </div>
