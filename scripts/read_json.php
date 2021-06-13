abab38e9c6e5fe929884bfbf590e24549202a034
Dennis4472373 <d.l.van.thulden@student.rug.nl>
2021-06-13 12:19:35 +0200

    Removed a lot of unnecessary and unused code.

--- scripts/read_json.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_POST['call_now'])) {
-    $json_file = file_get_contents("../data/test_sessions.json");
-
-    file_put_contents('./data/test_sessions.json', json_encode($json_file));
-}
-
